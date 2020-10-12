dd8af86f9e60bac0ff338d92a5c155d28894137e
clean-code-craft <cleancodcraft@gmail.com>
2020-10-12 11:06:46 +0530

    Updated

---
+++ ValidationLibrary/App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    
+    
+    public class Employee
+    {
+        [ValidationLibrary.RequiredAttribute]
+        
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee _emp = new Employee();
+
+            ValidationLibrary.Validator _validator = new ValidationLibrary.Validator();
+            _validator.Validate(_emp);
+        }
+    }
+}
---
+++ ValidationLibrary/ValidationLibrary/Validator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationLibrary
+{
+
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple =false)]
+    public class RequiredAttribute:System.Attribute
+    {
+        //
+        public bool Validate(object value)
+        {
+
+            string data = value as string;
+            return !string.IsNullOrEmpty(data);
+        }
+    }
+    public class Validator
+    {
+        public bool Validate(object target)
+        {
+            bool isValid = false;
+
+            //Discover Object Type Details (Reflection)
+            //Reflection :- API to query assembly metadata @runtime
+
+           System.Type _typeDetails= target.GetType();
+            if (_typeDetails.IsClass)
+            {
+                System.Reflection.PropertyInfo[] properties = _typeDetails.GetProperties();
+                foreach(var property in properties)
+                {
+                    Console.WriteLine($"{property.Name} && {property.GetValue(target)}");
+                    //Query Property Metdata for the existance of Custom Attribute - RequiredAttribute
+                   RequiredAttribute[] reqAttributes= property.GetCustomAttributes(typeof(RequiredAttribute), true) as RequiredAttribute[];
+                    if ( reqAttributes!=null && reqAttributes.Length > 0)
+                    {
+                        RequiredAttribute _instance = reqAttributes[0];
+                        if (_instance.Validate(property.GetValue(target)))
+                        {
+                            isValid = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("target is not an instance of class");
+            }
+            return isValid;
+
+        }
+    }
+}
