e3a97b5213ea79b5cf936c779148cfd0b969c5e0
clean-code-craft <cleancodcraft@gmail.com>
2020-10-13 10:55:48 +0530

    Why Linq

---
+++ ValidationLibrary/ReuseProbs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReuseProbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> resultList = new List<string>();
+            string[] names = { "Bnil", "Venu", "Sudeep", "Anupama" };
+            for(int i = 0; i < names.Length; i++)
+            {
+                if(names[i].Length > 4)
+                {
+                    resultList.Add(names[i]);
+                }
+            }
+
+            foreach (var item in resultList)
+            {
+                Console.WriteLine(item);
+
+            }
+
+        }
+    }
+}
