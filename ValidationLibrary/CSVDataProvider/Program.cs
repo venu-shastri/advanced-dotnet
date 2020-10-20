1ac8a6f60e460bbbe6c9b09c030b30b1f9769305
clean-code-craft <cleancodcraft@gmail.com>
2020-10-20 13:09:13 +0530

    CSV Updated

--- ValidationLibrary/CSVDataProvider/Program.cs
+++ ValidationLibrary/CSVDataProvider/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,29 @@ using System.Threading.Tasks;
 
 namespace CSVDataProvider
 {
+    public class Patient
+    {
+
+        public string MRN { get; set; }
+        public string Name { get; set; }
+        public int Age{ get; set; }
+        public string ContactNumber { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            CSVLinqDataProvider _provider = new CSVLinqDataProvider();
+           IEnumerable<Patient> _patientList= _provider.GetData("..//..//patients.csv");
+
+            var result = from p in _patientList
+                         where p.Age > 35
+                         select new { Mrn = p.MRN, Name = p.Name };
+            foreach(var item in result)
+            {
+                Console.WriteLine($"{item.Mrn} , {item.Name}");
+            }
+
         }
     }
 }
