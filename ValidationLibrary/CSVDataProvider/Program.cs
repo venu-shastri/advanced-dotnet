25a2529599d795f5a9f85d0f81056df7f51848ab
clean-code-craft <cleancodcraft@gmail.com>
2020-10-21 11:24:15 +0530

    Dynamic Updated

--- ValidationLibrary/CSVDataProvider/Program.cs
+++ ValidationLibrary/CSVDataProvider/Program.cs
@@ -1,35 +1,94 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSVDataProvider
 {
-    public class Patient
-    {
-
-        public string MRN { get; set; }
-        public string Name { get; set; }
-        public int Age{ get; set; }
-        public string ContactNumber { get; set; }
-    }
+   
     class Program
     {
         static void Main(string[] args)
         {
             CSVLinqDataProvider _provider = new CSVLinqDataProvider();
-           IEnumerable<Patient> _patientList= _provider.GetData("..//..//patients.csv");
+           List<CSVLine> _patientList= _provider.GetData("..//..//patients.csv").ToList();
 
-            var result = from p in _patientList
-                         where p.Age > 35
-                         select new { Mrn = p.MRN, Name = p.Name };
+            var result = from dynamic line  in _patientList
+                         where Int32.Parse( line.Age) > 35
+                         select new { Mrn = line.Mrn, Name = line.Name };
             foreach(var item in result)
             {
                 Console.WriteLine($"{item.Mrn} , {item.Name}");
             }
 
         }
+    }
+
+    public class CSVLine : System.Dynamic.DynamicObject
+    {
+        List<string> _headerContent;
+        List<string> _lineContent;
+        public CSVLine(string header, string line)
+        {
+            _headerContent = header.Split(',').ToList();
+            _lineContent = line.Split(',').ToList();
+
+        }
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = null;
+
+            int position=_headerContent.IndexOf(binder.Name);
+            if (position < _lineContent.Count)
+            {
+                object value = _lineContent[position];
+                result = value;
+                return true;
+            }
+
+            return false;
+        }
+
+
+    }
+    public class CSVLinqDataProvider
+    {
+        public  IEnumerable<CSVLine> GetData(string filePath)
+        {
+            List<CSVLine> _lineList = new List<CSVLine>();
+            string headerLine = default(string);
+            if (ValidatePath(filePath))
+            {
+                bool isHeaderPorcessed = false;
+                System.IO.StreamReader rd = new System.IO.StreamReader(filePath);
+                do
+                {
+                    if (!isHeaderPorcessed)
+                    {
+                        headerLine=rd.ReadLine();
+                        isHeaderPorcessed = true;
+                        
+                    }
+                    else {
+
+                        CSVLine _lineObj = new CSVLine(headerLine, rd.ReadLine());
+                        _lineList.Add(_lineObj);
+                    }
+                }
+                while (!rd.EndOfStream) ;
+                
+            }
+            return _lineList;
+        }
+
+        bool ValidatePath(string filePath) { return true; }
+
+        
+
+        
+
     }
 }
---
+++ ValidationLibrary/DynamicKeyword/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicKeyword
+{
+    public class StateBag:DynamicObject
+    {
+        Dictionary<string, object> _propertyStore = new Dictionary<string, object>();
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = null;
+            if (_propertyStore.ContainsKey(binder.Name))
+            {
+                result = _propertyStore[binder.Name];
+                    
+                return true;
+            }
+            return false;
+        }
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            _propertyStore[binder.Name] = value;
+            return true;
+        }
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            result = null;
+            if (binder.Name == "ClearAll")
+            {
+                _propertyStore.Clear();
+                return true;
+            }
+            return false;
+
+        }
+
+    }
+    public class A:System.Dynamic.DynamicObject
+    {
+
+        public void Task() { }
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            result = null;
+            switch (binder.Name)
+            {
+                case "Test1":break;
+            
+            }
+            return true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Client(new A());
+
+            dynamic _customer = new StateBag();
+            _customer.Id = "Cust001";
+            _customer.Name = "Tom";
+            _customer.Age = 30;
+            _customer.Address = "BLR";
+
+            Console.WriteLine($"{_customer.Id} , {_customer.Name},{_customer.Age}, {_customer.Address}");
+            _customer.Pan = "ADYPV0026G";
+            Console.WriteLine($"{_customer.Id} , {_customer.Name},{_customer.Age}, {_customer.Address},{_customer.Pan}");
+            _customer.ClearAll();
+
+
+
+
+               
+            
+            
+        }
+        static void Client(dynamic obj)
+        {
+            //Reflection Code 
+            //obj.GetType().GetMethod("Task").Invoke(obj, new object[] { });
+            obj.Task1(10,20); // MSIL - RuntimeBinder Code 
+        }
+    }
+}
